3003dfe16ee0fd1d1e1e6ac4d45941e5de34126f
Usman Rahat <engr.usmanrahat93@gmail.com>
2023-07-04 12:22:03 +0930

    Tutorial_2 content added.

---
+++ Tutorial_2/CallStack/Methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorial_2.CallStack
+{
+    public class Methods
+    {
+        public static void Method1()
+        {
+            Console.WriteLine("Start Method: Method 1");
+            Method2();
+            Console.WriteLine("Finish Method: Method 1");
+
+        }
+
+        public static void Method2()
+        {
+            Console.WriteLine("Start Method: Method 2");
+            Method3();
+            Console.WriteLine("Finish Method: Method 2");
+
+        }
+
+        public static void Method3()
+        {
+            Console.WriteLine("Start Method: Method 3");
+        }
+    }
+}
---
+++ Tutorial_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Tutorial_2.CallStack;
+
+//Explain Call Stack
+namespace Tutorial_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Method: Main");
+            Methods.Method1();
+            Console.WriteLine("Finish Method: Main");
+        }
+           
+        
+
+    }
+}
